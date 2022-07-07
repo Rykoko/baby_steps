854cc32d1d5da7ab2abb4c31176233c8ae9ee3d8
Rykoko <rxgcoyle@gmail.com>
2022-07-07 11:05:13 +0200

    Encode input data

---
+++ code_generator.py
@@ -0,0 +1,17 @@
+import random
+
+message = input('What would you like to encode?: ')
+
+
+def encode(word):
+    replacements = [('a', '£'), ('e', '$'), ('i', '€'), ('o', '§'), ('u', '@'),
+                    ('b', '1'), ('c', '2'), ('d', '3'), ('f', '4'), ('g', '5'),
+                    ('h', '6'), ('j', '#'), ('k', '7'), ('l', '8'), ('m', '9'),
+                    ('n', '!'), ('p', ';'), ('r', '^'), ('s', '/'), ('t', '"'),
+                    ('v', '*'), ('w', 'ç'), ('x', '&'), ('y', '£'), ('z', '?')]
+    for (x, y) in replacements:
+        word = word.replace(x, y)
+    return word
+
+
+print(encode(message))
