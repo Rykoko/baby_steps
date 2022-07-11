b63cc1fde38e713622d3fb41a1389f1aa72c1564
Rykoko <rxgcoyle@gmail.com>
2022-07-11 22:12:51 +0200

    Update auto_speed.py

--- auto_speed.py
+++ auto_speed.py
@@ -4,6 +4,10 @@ import time
 speed = speedtest.Speedtest()
 loop_count = 0
 
+ds_list = []
+us_list = []
+ping_list = []
+
 
 def humansize(nbits):
     suffixes = ['b/s', 'Kb/s', 'Mbit/s', 'Gb/s', 'Tb/s', 'Pb/s']
@@ -18,18 +22,21 @@ def humansize(nbits):
 entry = input('Hit enter to run test')
 if len(entry) < 1:
 
-    while loop_count < 2:
+    while loop_count < 100:
 
         print('Loop: ', loop_count + 1)
 
         ds = speed.download()
+        ds_list.append(ds)
         print('\n', 'Download speed: ', humansize(ds), '\n')
 
         us = speed.upload()
+        us_list.append(us)
         print('\n', 'Upload speed: ', humansize(us), '\n')
 
         servernames = []
         speed.get_servers(servernames)
+        ping_list.append(ping_list)
         print('\n', 'Ping:', speed.results.ping, 'ms\n')
 
         loop_count += 1
@@ -37,3 +44,24 @@ if len(entry) < 1:
         time.sleep(30)
 
 print('loops: ', loop_count)
+print(ds_list)
+print(us_list)
+print(ping_list)
+
+for i in ds_list:
+    i += i
+    print(i)
+    x = i/len(ds_list)
+print('Average download speed: ', x)
+
+for j in us_list:
+    j += j
+    print(j)
+    y = j/len(ds_list)
+print('Average upload speed: ', y)
+
+for l in ping_list:
+    l += l
+    print(l)
+    z = l/len(ds_list)
+print('Average ping: ', z)
